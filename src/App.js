398575d18e1ae65b8ba1a7d952addc63635cfb05
uğur kıymetli <ugurkiymetli@hotmail.com>
2023-01-01 22:31:05 +0300

    create react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to <code>QR Menu</code> App!.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
