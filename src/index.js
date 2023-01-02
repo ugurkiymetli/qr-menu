1d93f41e830996265b19a8e1f4f4fb04e091a0ce
uğur kıymetli <ugurkiymetli@hotmail.com>
2023-01-02 03:22:47 +0300

    add react query, axios

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { HashRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import App from './App';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </HashRouter>
 );
