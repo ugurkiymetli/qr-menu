4aec49416b19d207c796aac9c2ab6b85c98e0afa
uğur kıymetli <ugurkiymetli@hotmail.com>
2023-01-02 20:10:36 +0300

    add loading indicator

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Loading.css';
+
+export default function Loading() {
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner" />
+    </div>
+  );
+}
