f345b405171e0d92d69d4ea9a987968b787dee6c
uğur kıymetli <ugurkiymetli@hotmail.com>
2023-01-25 21:38:28 +0300

    add dotenv and test secret

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,15 +1,31 @@
+import { Grid, Tooltip } from '@mui/material';
 import React from 'react';
 import './Main.css';
 
 function Main() {
+  const secret = process.env.REACT_APP_SECRET;
+
+  console.log(secret);
+
+  const mainStyle = {
+    textAlign: 'center',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    color: 'text.primary',
+    fontSize: 'calc(10px + 2vmin)',
+  };
   return (
-    <div className="main">
+    <Grid sx={mainStyle}>
       <p>
-        Welcome to
+        <Tooltip title={secret}>
+          <span>Welcome to</span>
+        </Tooltip>
         <code className="main-link"> QR Menu </code>
         App!.
       </p>
-    </div>
+    </Grid>
   );
 }
 
